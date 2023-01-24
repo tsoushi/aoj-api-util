e45e90404f886e3412c9cecba0845f3944195ab0
tsoushi <79128170+tsoushi@users.noreply.github.com>
2023-01-24 21:39:13 +0900

    C++,C++11,C++14,C++17のコメント削除に対応

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 import re
 
 def removeComments(text: str, language: str):
-    if language == 'C':
+    if language in ['C', 'C++', 'C++11', 'C++14', 'C++17']:
         return re.sub(r'/\*[\s\S]*?\*/|//.*', '', text)
     raise Exception(f'コメントの削除に対応していない言語です : {language}')
